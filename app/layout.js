dbb197826182203a9a87d1ad351dbb8f5277f094
Codev <codev@macbookair-2.home>
2026-09-18 22:06:22 +0200

    Ajout Vercel Analytics et Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import "./globals.css";
@@ -18,8 +21,13 @@ export default function RootLayout({ children }) {
     <html lang="fr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
-          {children}
+
+        {children}
+
         <Footer />
+
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
